d4961f89a48c80bc35a102f0d97392f2c8263591
ishmahaida <47313160+ishmahaida@users.noreply.github.com>
2020-02-14 18:50:48 +0700

    Create JS-001.js

---
+++ JS-001.js
@@ -0,0 +1,13 @@
+function grade(point){
+    if( point >= 90 ){
+        return = "A" 
+    }else if(point >= 80 ){
+        return = "B"
+    }else if( point >= 70 ){
+        return = "C"
+    }else if(point >=60 ){
+        return = "D"
+    }else if(point <= 59){
+        return = "D"
+    }
+}
